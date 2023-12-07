8859ccfe4b626115fa0e4f882447a0c761c1346e
ANDREEA ARON <andreea.aron1@stud.ubbcluj.ro>
2023-12-07 12:10:35 +0200

    Laboratorul complet + tema. Tema: in ShoppingListDatabase, am creat metodele GetAllProducts si DeleteListProductAsync; in ListPage.xaml am creat butonul Delete Item; in ListPage.xaml.cs am creat metoda OnDeleteItemButtonClicked

--- ListPage.xaml.cs
+++ ListPage.xaml.cs
@@ -20,4 +20,42 @@ public partial class ListPage : ContentPage
         await App.Database.DeleteShopListAsync(slist);
         await Navigation.PopAsync();
     }
+
+    async void OnDeleteItemButtonClicked(object sender, EventArgs e)
+    {
+        var slist = (ShopList)BindingContext;
+        if(listView.SelectedItem != null)
+        {
+            var selected_Product = listView.SelectedItem as Product;
+            var all_products = await App.Database.GetAllProducts();
+            var toDelete = all_products.Find(x => x.ProductID == selected_Product.ID && x.ShopListID == slist.ID);
+        
+            if(toDelete != null)
+            {
+                await App.Database.DeleteListProductAsync(toDelete);
+                await Navigation.PopAsync();
+            }
+        }
+        
+    }
+
+    async void OnChooseButtonClicked(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new ProductPage((ShopList)
+       this.BindingContext)
+        {
+            BindingContext = new Product()
+        });
+
+    }
+
+    protected override async void OnAppearing()
+    {
+        base.OnAppearing();
+        var shopl = (ShopList)BindingContext;
+
+        listView.ItemsSource = await App.Database.GetListProductsAsync(shopl.ID);
+    }
+
+
 }
